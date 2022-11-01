fe57c139140b657d0e33ed0a17cd70c9504cda3f
Marcos Lott <lott.marcos@gmail.com>
2022-10-31 22:39:47 -0300

    setting up the  database and the  back end connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Axios from 'axios';
 import './App.css';
 
 function App() {
+  const [movie_name, set_movie_name] = useState('')
+  const [movie_review, set_review] = useState('')
+  const [reviews_list, set_reviews_list] = useState([])
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/api/get').then((response) =>{
+      set_reviews_list(response.data)
+    })
+  }, []);
+
+  const submit_review = () => {
+    Axios.post('http://localhost:3001/api/insert', {
+      movie_name: movie_name, 
+      movie_review: movie_review,
+    }).then(() => {
+      alert('successful insert');
+    });
+  };
+
   return (
     <div className="App">
       <h1>CRUD APPLICATION</h1>
       <div className="form">
         <label>Movie Name</label>
-        <input type="text" name="movieName" />
+        <input type="text" name="movieName" onChange={(e) => {
+          set_movie_name(e.target.value)
+        }}/>
         <label>Review</label>
-        <input type="text" name="review" />
-        <button type="button">Submit</button>
+        <input type="text" name="review" onChange={(e) => {
+          set_review(e.target.value)
+        }}/>
+        <button type="button" onClick={submit_review}>Submit</button>
+        {reviews_list.map((val) =>{
+          return (
+            <h1 key={val.id}>
+              Movie Name: {val.movie_name} | Review: {val.movie_review}
+            </h1>
+          )
+        })}
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,10 +1,40 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const app = express();
+const mysql = require('mysql');
 
-app.get("/", (req, res) => {
-   res.send("hello");
+const db = mysql.createPool({
+   host: 'localhost',
+   user: 'lott',
+   password: '9503',
+   database: 'cruddb',
+})
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/api/get", (req, res) => {
+
+   const sql_select = 
+      "SELECT * FROM movie_reviews;"
+   db.query(sql_select, (err, result) => {
+      res.send(result)
+   })
+});
+
+app.post("/api/insert", (req, res) => {
+   const movie_name = req.body.movie_name
+   const movie_review = req.body.movie_review
+
+   const sql_insert = "INSERT INTO movie_reviews (movie_name, movie_review) VALUES (?, ?);"
+   db.query(sql_insert, [movie_name, movie_review], (err, result) => {
+      console.log(result)
+   })
 });
 
 app.listen(3001, () => {
    console.log("running on port 3001");
 });
+
