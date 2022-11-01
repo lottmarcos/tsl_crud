8c4c3169e46c3483ca2b3c9e0038d6da880333be
Marcos Lott <lott.marcos@gmail.com>
2022-11-01 18:58:34 -0300

    finishing the project

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,38 @@ import Axios from 'axios';
 import './App.css';
 
 function App() {
-  const [movie_name, set_movie_name] = useState('')
-  const [movie_review, set_review] = useState('')
+  const [movie_name, set_movie_name] = useState("")
+  const [movie_review, set_review] = useState("")
+  const [new_review, set_new_review] = useState("")
   const [reviews_list, set_reviews_list] = useState([])
 
   useEffect(() => {
-    Axios.get('http://localhost:3001/api/get').then((response) =>{
+    Axios.get("http://localhost:3001/api/get").then((response) =>{
       set_reviews_list(response.data)
     })
   }, []);
 
   const submit_review = () => {
-    Axios.post('http://localhost:3001/api/insert', {
+    Axios.post("http://localhost:3001/api/insert", {
       movie_name: movie_name, 
       movie_review: movie_review,
-    }).then(() => {
-      alert('successful insert');
     });
+
+    set_reviews_list([
+      ...reviews_list, 
+      { movie_name: movie_name, movie_review: movie_review }
+    ]);
+  };
+
+  const delete_review = (movie) => {
+    Axios.delete("http://localhost:3001/api/delete/", {data: {movie_name: movie}});
+  };
+
+  const update_review = (movie) => {
+    Axios.put("http://localhost:3001/api/update/", {
+      movie_name: movie,
+      movie_review: new_review,
+  });
   };
 
   return (
@@ -37,9 +52,18 @@ function App() {
         <button type="button" onClick={submit_review}>Submit</button>
         {reviews_list.map((val) =>{
           return (
-            <h1 key={val.id}>
-              Movie Name: {val.movie_name} | Review: {val.movie_review}
-            </h1>
+            <div className="card"> 
+              <h3 key={val.id}>{val.movie_name}</h3>
+              <p>{val.movie_review}</p>
+              <button type="button" onClick={() => {delete_review(val.movie_name)}}>Delete</button>
+              <input type="text" id="update-text" onChange={(e) => {
+                set_new_review(e.target.value)
+              }}/>
+              <button type="button" onClick={() => {
+                if (new_review.length > 0)
+                update_review(val.movie_name)}
+                }>Update</button>
+            </div>
           )
         })}
       </div>
--- server/index.js
+++ server/index.js
@@ -30,7 +30,26 @@ app.post("/api/insert", (req, res) => {
 
    const sql_insert = "INSERT INTO movie_reviews (movie_name, movie_review) VALUES (?, ?);"
    db.query(sql_insert, [movie_name, movie_review], (err, result) => {
-      console.log(result)
+      if (err) console.log(err)
+   })
+});
+
+app.delete("/api/delete", (req, res) => {
+   const movie_name = req.body.movie_name
+   const sql_delete = "DELETE FROM movie_reviews WHERE movie_name = ?;"
+
+   db.query(sql_delete, movie_name, (err, result) => {
+      if (err) console.log(err)
+   })
+});
+
+app.put("/api/update", (req, res) => {
+   const movie_name = req.body.movie_name
+   const movie_review = req.body.movie_review
+   const sql_update = "UPDATE movie_reviews SET movie_review = ? WHERE movie_name = ?;"
+
+   db.query(sql_update, [movie_review, movie_name], (err, result) => {
+      if (err) console.log(err)
    })
 });
 
